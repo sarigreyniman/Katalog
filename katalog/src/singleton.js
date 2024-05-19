f7626a7a6afd0a6f17d63feafd495e1acb2cad30
sarigreyniman <sari0534170279@gmail.com>
2024-05-19 03:27:50 +0300

    saf

--- katalog/src/singleton.js
+++ katalog/src/singleton.js
@@ -22,14 +22,36 @@ class Singleton {
         this.getList();
     }
 
+    // async getList() {
+    //     try {
+    //         const response = await axios.get(apiUrl);
+    //         runInAction(() => {
+    //             this.List = response.data;
+    //             console.log(this.List);
+
+    //         });
+    //     } catch (error) {
+    //         console.log("Error getting users:", error);
+    //     }
+    // }
     async getList() {
         try {
             const response = await axios.get(apiUrl);
             runInAction(() => {
-                this.List = response.data;
+                // הבדיקה הראשונה היא לוודא שהנתונים שמוחזרים הם מערך
+                if (Array.isArray(response.data)) {
+                    // הבדיקה השנייה היא לוודא שכל ערך במערך הוא עובד (worker)
+                    if (response.data.every(worker => typeof worker === 'object' && worker !== null)) {
+                        this.List = response.data;
+                    } else {
+                        console.error("Error getting users: response data is not an array of workers");
+                    }
+                } else {
+                    console.error("Error getting users: response data is not an array");
+                }
             });
         } catch (error) {
-            console.log("Error getting users:", error);
+            console.error("Error getting users:", error);
         }
     }
 
